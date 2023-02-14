ce8f8a953c7828c0453fd33cfc8abcd2bf8bd823
Mouhamed-git <m.diackk@gmail.com>
2023-02-14 13:27:29 +0000

    chore:update pg-promise version

--- config.js
+++ config.js
@@ -42,7 +42,11 @@ switch (process.env.STAGE){
         config = config_devel;
 }
 
+
 // Build connection string
 config.db.connectionString = config.db.server + "/" + config.db.database
+
+console.log("config", config.db.connectionString);
+
 
 module.exports = config;
